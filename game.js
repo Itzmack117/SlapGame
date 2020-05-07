789faf92858fb18e2976f5dc4d4dcca884dfc6da
itzmack117 <mackjones95@gmail.com>
2020-05-07 17:15:35 -0600

    Alpha Build

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 let hits = 0
+let staffMod = 0
+let robeMod = 0
 let items = {
     staff: {
         name: "newStaff",
         modifier: 2,
-        description: "+10 holy damage",
+        description: "+2 holy damage",
+
     }, robes: {
         name: "whiteRobes",
         modifier: 5,
@@ -18,35 +21,55 @@ let gandalf = {
     items: []
 }
 
-function giveStaff() {
-    gandalf.items.push(items.staff)
+// function giveStaff() {
+//     gandalf.items.push(items.staff)
+//     console.log(gandalf.items)
+//     gandalf.items.forEach(s => { staffMod += s.modifier })
+//     console.log(staffMod)
+//     document.getElementById("hideStaff").classList.add("hide")
+//     return staffMod
+// }
+// function giveRobes() {
+//     gandalf.items.push(items.robes)
+//     console.log(gandalf.items)
+//     gandalf.items.forEach(s => { robeMod += s.modifier })
+//     console.log(robeMod)
+//     document.getElementById("hideRobes").classList.add("hide")
+//     return robeMod
+// }
+// let mods = staffMod + robeMod
+
+let mods = 0
+function giveItem(itemName) {
+    mods = 0
+    gandalf.items.push(items[itemName])
     console.log(gandalf.items)
+    gandalf.items.forEach(s => { mods += s.modifier })
+    console.log(mods)
 }
-giveStaff()
+function hide() {
+}
 function slap() {
-    balrog.health--
+    balrog.health -= 1 + mods
     console.log(balrog.health)
     hits++
-    if (balrog.health == 0) {
-    }
+    checkHealth()
     update()
 }
 
 function punch() {
-    balrog.health -= 5
+    balrog.health -= 5 + mods
     console.log(balrog.health)
     hits++
-    if (balrog.health == 0) {
-    }
+    checkHealth()
     update()
 }
 
 function kick() {
-    balrog.health -= 10
+    balrog.health -= 10 + mods
     console.log(balrog.health)
     hits++
-    if (balrog.health == 0) {
-    }
+    checkHealth()
     update()
 }
 
@@ -55,5 +78,12 @@ function update() {
     document.getElementById("hits").innerText = hits.toString()
     console.log(hits)
 }
+function checkHealth() {
+    if (balrog.health < 1) {
 
+        document.getElementById('victory-message').innerHTML = `<p>The Balrog is slain!!</p>`
+        balrog.health = 0
+        hits--
+    }
+}
 update()
